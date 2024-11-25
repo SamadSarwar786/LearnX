2e9fceff4af62d66dce7e6a3551793b64609acb2
Samad sarwar <samad.s@abhibus.com>
2024-11-26 01:16:14 +0530

    init commit

---
+++ src/main/java/com/learnx/learnx_backend/LearnXBackendApplication.java
@@ -0,0 +1,13 @@
+package com.learnx.learnx_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnXBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnXBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learnx/learnx_backend/LearnXBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learnx.learnx_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnXBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
