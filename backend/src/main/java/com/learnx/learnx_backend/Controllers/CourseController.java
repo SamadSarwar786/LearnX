91f7f45ebd73afbd2bfc71721810900820dc109e
Samad sarwar <samad.s@abhibus.com>
2025-01-14 13:30:21 +0530

    convert courseRes to course

--- backend/src/main/java/com/learnx/learnx_backend/Controllers/CourseController.java
+++ backend/src/main/java/com/learnx/learnx_backend/Controllers/CourseController.java
@@ -1,6 +1,6 @@
 package com.learnx.learnx_backend.Controllers;
 
-import com.learnx.learnx_backend.Dtos.ResponseDtos.CourseResDto;
+import com.learnx.learnx_backend.Models.Course;
 import com.learnx.learnx_backend.Services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -16,7 +16,7 @@ public class CourseController {
     private CourseService courseService;
 
     @GetMapping
-    public Page<CourseResDto> getAllCourses(
+    public Page<Course> getAllCourses(
             @RequestParam(value = "page", defaultValue = "0") int page,
             @RequestParam(value = "size", defaultValue = "10") int size) {
         return courseService.getAllCourses(page,size);
