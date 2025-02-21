2cd9842bd311755ec550e37b870bb686ea11e909
Samad Sarwar <samad.s@abhibus.com>
2025-02-22 01:55:41 +0530

    update the baseUrl from http to https

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://learnx.me/",
+    baseUrl: "https://learnx.me/",
     prepareHeaders: (headers, { getState, endpoint }) => {
       // headers.set("X-APP-NAME", process.env.REACT_APP_CODEX || "");
       if (!headers.has("Content-Type"))
